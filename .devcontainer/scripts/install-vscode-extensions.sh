edfa42628b50848fb70542b79f7b1c839e151f7d
Maksim Kamanin <ltcatyl@gmail.com>
2023-03-04 23:15:07 +0300

    extensions pre-installation implemented

--- .devcontainer/scripts/install-vscode-extensions.sh
+++ .devcontainer/scripts/install-vscode-extensions.sh
@@ -1,3 +1,14 @@
-for extension in $(jq '.customizations.vscode.extensions' ../devcontainer.json); do
-  code server --install-extension $extension
+#!/bin/bash
+
+# Please make sure that download-vscode-server.sh script
+# downloads only one commit in ~/.vscode-server/bin folder
+
+commit_sha=$(ls /root/.vscode-server/bin)
+export PATH="$PATH:/root/.vscode-server/bin/$commit_sha/bin"
+
+for line in $(jq '.customizations.vscode.extensions' .devcontainer/devcontainer.json); do
+  if [[ $line != '[' && $line != ']' ]]; then
+    extension=$(echo $line | tr -d '",')
+    code-server --install-extension $extension --force
+  fi
 done
